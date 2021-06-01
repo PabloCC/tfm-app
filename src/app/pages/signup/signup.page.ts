5138ecf5b888ce2c364aa541918473e3d1ae01a1
pablocc <pablocc1996@gmail.com>
2021-06-01 12:19:22 +0200

    First version signUp form

--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor() { 
+    this.signUpForm = new FormGroup({
+      username: new FormControl('', [
+        Validators.required,
+      ]),
+      email: new FormControl('', [
+        Validators.required,
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+      ]),
+      role: new FormControl('', [
+        Validators.required,
+      ]),
+    });
+  }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    const body = {
+      username: this.signUpForm.get('username').value,
+      email: this.signUpForm.get('email').value,
+      password: this.signUpForm.get('password').value,
+      role: this.signUpForm.get('role').value,
+    }
+  }
 }

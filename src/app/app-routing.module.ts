c094e2a8b708f53739e566ae2424527774bbcd0b
pablocc <pablocc1996@gmail.com>
2021-06-03 21:28:40 +0200

    Refactor auth modules files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
-import { Role } from './core/role.enum';
+import { Role } from './core/models/role.enum';
 import { RoleGuard } from './core/guards/role.guard';
 
 const routes: Routes = [
